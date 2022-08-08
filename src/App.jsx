be416448b1e376614f62198005dad37dee20c476
Eugene Kozakov <lointainyyy@gmail.com>
2022-08-08 12:45:45 +0300

    created basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,20 @@
+/* Components */
+import { CountryList } from './components/CountryList'
+import { Header } from './components/Header'
+import { SearchBar } from './components/SearchBar'
+/* Page`s */
+import { DetailsPage } from './page/DetailsPage'
+import { MainPage } from './page/MainPage'
+
 export const App = () => {
-  return <>app</>
+  return (
+    <>
+      <Header />
+
+      <SearchBar />
+
+      <MainPage />
+      <DetailsPage />
+    </>
+  )
 }
