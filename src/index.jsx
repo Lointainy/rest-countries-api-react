c9486e5f9ee48e76fd1c008a5dcff9ea2471471d
Eugene Kozakov <lointainyyy@gmail.com>
2022-08-08 11:51:49 +0300

    added fontsource, path to font

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { App } from './App'
+
+/* STYLE */
+import '@/assets/scss/main.scss'
+
+/* font */
+import '@fontsource/nunito-sans'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
