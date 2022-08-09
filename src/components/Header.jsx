321e1b53e1dbfa781c74d9ad1658066b3a82812e
Eugene Kozakov <lointainyyy@gmail.com>
2022-08-09 10:26:23 +0300

    added fontawesome, path to icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 export const Header = () => {
   const [userTheme, setUserTheme] = useState('light-theme')
 
@@ -20,13 +22,23 @@ export const Header = () => {
   }, [userTheme])
 
   return (
-    <div className="header bg-secondary shadow-primary h-20">
+    <div className="header bg-secondary shadow-primary h-20 text-primary">
       <div className="header__content container flex justify-between h-full items-center">
-        <div className="header__logo">
+        <div className="header__logo text-2xl font-extrabold">
           <Link to="/">Where in the world?</Link>
         </div>
-        <button onClick={() => toggleTheme()} className="header__switch-toggle">
-          {userTheme !== 'dark-theme' ? <>Dark</> : <>Light</>}
+        <button onClick={() => toggleTheme()} className="header__theme-toggle flex items-center">
+          {userTheme !== 'dark-theme' ? (
+            <>
+              <FontAwesomeIcon className="header__theme-toggle-icon mr-2" icon="moon" />
+              Dark mode
+            </>
+          ) : (
+            <>
+              <FontAwesomeIcon className="header__theme-toggle-icon mr-2" icon="sun" />
+              Light mode
+            </>
+          )}
         </button>
       </div>
     </div>
--- src/index.jsx
+++ src/index.jsx
@@ -11,6 +11,12 @@ import './index.css'
 /* font */
 import '@fontsource/nunito-sans'
 
+/* Icons */
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faMoon, faSearch, faSun } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faMoon, faSun, faSearch)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
