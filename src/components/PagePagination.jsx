c1ac2289452dae4bb2ca1315d40cf112b01f546e
Eugene Kozakov <lointainyyy@gmail.com>
2022-08-14 20:04:43 +0300

    fix logic for disabled button

--- src/components/PagePagination.jsx
+++ src/components/PagePagination.jsx
@@ -9,7 +9,7 @@ export const PagePagination = () => {
     <div className="flex justify-center items-center md:w-auto sm:w-full sm:my-10 md:m-0 my-10 w-full">
       <PagePaginationButton disabledButton={currentPage <= 1} pageNumber={'prev'} iconName={'chevron-left'} />
       <PagePaginationButton pageNumber={1} disabledButton={currentPage === 1} />
-      <PagePaginationButton pageNumber={currentPage} />
+      <PagePaginationButton pageNumber={currentPage} disabledButton={currentPage === 1 || currentPage >= totalPage} />
       <PagePaginationButton pageNumber={totalPage} disabledButton={currentPage === totalPage} />
       <PagePaginationButton disabledButton={currentPage >= totalPage} pageNumber={'next'} iconName={'chevron-right'} />
     </div>
