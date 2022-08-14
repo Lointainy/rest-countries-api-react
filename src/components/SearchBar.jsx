cc61259ed416d88ee64aca8321a65270e76579cd
Eugene Kozakov <lointainyyy@gmail.com>
2022-08-14 14:09:02 +0300

    added style search bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,10 +4,10 @@ import { Search } from './Search'
 
 export const SearchBar = () => {
   return (
-    <>
+    <div className="searchbar container mt-12 flex flex-wrap justify-between items-center sm:p-0 px-4">
       <Search />
       <PagePagination />
       <Filter />
-    </>
+    </div>
   )
 }
