776ff486fc205b7c9d56402e1df0fa0d16f9c1f0
Eugene Kozakov <lointainyyy@gmail.com>
2022-08-30 18:28:46 +0300

    added pagination logic

--- src/components/PagePagination.jsx
+++ src/components/PagePagination.jsx
@@ -1,16 +1,19 @@
-import { useContext } from 'react'
-import { CountryContext } from '../hooks/Context'
+import { useSelector } from 'react-redux'
 import { PagePaginationButton } from './PagePaginationButton'
 
 export const PagePagination = () => {
-  const { currentPage, totalPage } = useContext(CountryContext)
+  const currentPage = useSelector((store) => store.countries.currentPage)
+  const totalPage = useSelector((store) => store.countries.totalPage)
 
   return (
     <div className="flex justify-center items-center md:w-auto sm:w-full sm:my-10 md:m-0 my-10 w-full">
       <PagePaginationButton disabledButton={currentPage <= 1} pageNumber={'prev'} iconName={'chevron-left'} />
       <PagePaginationButton pageNumber={1} disabledButton={currentPage === 1} />
       <PagePaginationButton pageNumber={currentPage} disabledButton={currentPage === 1 || currentPage >= totalPage} />
-      <PagePaginationButton pageNumber={totalPage} disabledButton={currentPage === totalPage} />
+      <PagePaginationButton
+        pageNumber={totalPage}
+        disabledButton={currentPage === totalPage || (currentPage === 1 && totalPage === 1)}
+      />
       <PagePaginationButton disabledButton={currentPage >= totalPage} pageNumber={'next'} iconName={'chevron-right'} />
     </div>
   )
