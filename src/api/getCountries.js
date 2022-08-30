02f4c58ebb9d97bcb89c4e2774e2adae31a065ba
Eugene Kozakov <lointainyyy@gmail.com>
2022-08-30 18:43:08 +0300

    .

--- src/api/getCountries.js
+++
@@ -1,8 +0,0 @@
-import { BASE_URL } from './config'
-
-export const getCountries = async () => {
-  let countriesData = await fetch(`${BASE_URL}/all`)
-    .then((res) => res.json())
-    .then((data) => data)
-  return countriesData
-}
--- src/api/getCountry.js
+++
@@ -1,8 +0,0 @@
-import { BASE_URL } from './config'
-
-export const getCountry = async (countryName) => {
-  let countryData = await fetch(`${BASE_URL}/alpha/${countryName}`)
-    .then((res) => res.json())
-    .then((data) => data)
-  return countryData
-}
