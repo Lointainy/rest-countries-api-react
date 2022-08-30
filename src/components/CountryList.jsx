43f688dc28c8c0213257cff790df6f494c4a88ff
Eugene Kozakov <lointainyyy@gmail.com>
2022-08-30 18:28:57 +0300

    import store

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -1,14 +1,22 @@
-import { useContext } from 'react'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
-import { CountryContext } from '../hooks/Context'
 import { CountryCard } from './CountryCard'
 
+import { filteredCountries } from '../store/countriesSlice'
+
 export const CountryList = () => {
-  const { filteredListCountries = [] } = useContext(CountryContext)
+  const countries = useSelector((store) => store.countries.countries)
+  const dispatch = useDispatch()
+  const page = useSelector((store) => store.countries.currentPage)
+
+  useEffect(() => {
+    dispatch(filteredCountries())
+  }, [page])
 
   return (
-    <div className="container mt-12 grid grid-cols-1 gap-[4.5rem] place-content-center w-[16.5rem] sm:grid-cols-2 sm:w-[37.5rem] lg:grid-cols-3 lg:w-[58.5rem] xl:grid-cols-4 xl:w-full">
-      {filteredListCountries.map((item, index) => {
+    <div className="container my-12 grid grid-cols-1 gap-[4.5rem] place-content-center w-[16.5rem] sm:grid-cols-2 sm:w-[37.5rem] lg:grid-cols-3 lg:w-[58.5rem] xl:grid-cols-4 xl:w-full">
+      {countries.map((item, index) => {
         return (
           <Link key={index} to={`/details/${item.cca3.toLowerCase()}`}>
             <CountryCard country={item} />
